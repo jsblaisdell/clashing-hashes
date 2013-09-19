179fbc10ca8197a3c2cdb46a473f660bae32d07f
Justin Blaisdell <jsblaisdell@gmail.com>
2013-09-19 13:18:40 -0500

    add tweet hash to page

--- clashing-hashes.rb
+++ clashing-hashes.rb
@@ -2,12 +2,16 @@ require 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader'
+require_relative 'lib/twitter'
 
 get '/' do
   erb :index
 end
 
 post '/twitter' do
+  t  = Twitter::Tweet.new("testing...", "JustinBlaisdell")
+  t2 = Twitter::Tweet.new("derp", "nblais")
+  @tweets = [t, t2]
   @first  = params[:first_hash]
   @second = params[:second_hash]
   erb :twitter
